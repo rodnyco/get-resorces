c82c3b821108db10c29dcd5c965cfb2fa6c59c95
Владимир Раду <rodny_co@list.ru>
2021-10-02 13:18:19 +0300

    Inital

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MaterialContent from './components/MaterialContent';
+import MaterialForm from './components/MaterialForm.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="app-title">Поиск говна для уроков</h1>
       </header>
+      <MaterialForm/>
+      <MaterialContent/>
     </div>
   );
 }
---
+++ src/components/MaterialForm.js
@@ -0,0 +1,33 @@
+import { Form, Textarea, Button } from 'muicss/react';
+import { Grid } from '@material-ui/core';
+
+function MaterialForm() {
+    return (
+        <Form className="material-form">
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={4}>
+                    <div className="material-form__item lesson-topic">
+                        <h3>Тема урока</h3>
+                        <span>по русски пиши бля</span>
+                        <Textarea placeholder="Тему напиши" />
+                    </div>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <div className="material-form__item lesson-article">
+                        <h3>Статья</h3>
+                        <span>по которой тебя петушить будут</span>
+                        <Textarea placeholder="Пиши давай" />
+                    </div>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <p>Тут будут чекбоксы</p>
+                </Grid>
+            </Grid>
+            <Button variant="contained" color="primary" className="material-form button">
+                Ищем
+            </Button>
+        </Form>
+    );
+}
+
+export default MaterialForm;
